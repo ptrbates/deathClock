6abe4471fd86ed44a96cc82a4cc125fa5efaecee
Peter Bates <pbates@localhost.localdomain>
2020-03-22 11:24:33 -0700

    update App.js to use hooks

--- src/App.js
+++ src/App.js
@@ -1,43 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import Input from "./components/Input";
 import Output from "./components/Output";
 import "./App.css";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.onChange = this.onChange.bind(this);
-    this.state = {
-      birthYear: 2020,
-      birthMonth: 1,
-      gender: "",
-      race: ""
-    };
+function App() {
+  const [birthYear, setBirthYear] = useState(2020);
+  const [birthMonth, setBirthMonth] = useState(1);
+  const [gender, setGender] = useState("");
+  const [race, setRace] = useState("");
+
+  function onChange(name, value) {
+    switch (name) {
+      case "birthYear":
+        setBirthYear(value);
+        break;
+      case "birthMonth":
+        setBirthMonth(value);
+        break;
+      case "gender":
+        setGender(value);
+        break;
+      case "race":
+        setRace(value);
+        break;
+    }
   }
 
-  onChange(name, value) {
-    this.setState({ [name]: value });
-  }
-
-  render() {
-    return (
-      <div data-testid="main-app" className="row-container">
-        <Input
-          birthYear={this.state.birthYear}
-          birthMonth={this.state.birthMonth}
-          gender={this.state.gender}
-          race={this.state.race}
-          onChange={this.onChange}
-        />
-        <Output
-          birthYear={this.state.birthYear}
-          birthMonth={this.state.birthMonth}
-          gender={this.state.gender}
-          race={this.state.race}
-        />
-      </div>
-    );
-  }
+  return (
+    <div data-testid="main-app" className="row-container">
+      <Input
+        birthYear={birthYear}
+        birthMonth={birthMonth}
+        gender={gender}
+        race={race}
+        onChange={onChange}
+      />
+      <Output
+        birthYear={birthYear}
+        birthMonth={birthMonth}
+        gender={gender}
+        race={race}
+      />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Input.js
+++ src/components/Input.js
@@ -5,18 +5,20 @@ import RaceGenderSelect from "./RaceGenderSelect";
 function Input(props) {
   return (
     <div data-testid="input-box" className="column-container">
-      <h1>Enter some of your information:</h1>
-      <BirthDateSelect
-        birthYear={props.birthYear}
-        birthMonth={props.birthMonth}
-        onChange={props.onChange}
-      />
-      <br />
-      <RaceGenderSelect
-        gender={props.gender}
-        race={props.race}
-        onChange={props.onChange}
-      />
+      <legend>Enter some of your information:</legend>
+      <fieldset>
+        <BirthDateSelect
+          birthYear={props.birthYear}
+          birthMonth={props.birthMonth}
+          onChange={props.onChange}
+        />
+        <br />
+        <RaceGenderSelect
+          gender={props.gender}
+          race={props.race}
+          onChange={props.onChange}
+        />
+      </fieldset>
     </div>
   );
 }
